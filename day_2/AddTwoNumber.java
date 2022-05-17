4eb6d1ff4856f4afb65f6985ae7e4aa34608ac7c
unknown <singhjank95@gmail.com>
2022-05-17 18:52:29 +0000

    some comments

---
+++ day_2/AddTwoNumber.java
@@ -0,0 +1,16 @@
+package com.demo;
+ class AddTwoNumbes {
+    // method
+    void SumOfTwoNumbers() {
+        int number1 = 10;
+        int number2 = 20;
+        int result = number1 + number2;
+        System.out.print("Sum of two numbers " + result);
+    }
+}
+public class AddTwoNumber {
+    public static void main(String arg[]) {
+        AddTwoNumbes object = new AddTwoNumbes();// object of this class -> AddTwoNumber
+        object.SumOfTwoNumbers();// calling from here
+    }
+}
